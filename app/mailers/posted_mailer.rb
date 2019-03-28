778ca19204e8aa848095efa7125ecdb44c5d1b11
若山侑輝 <wkym_4a@icloud.com>
2019-03-28 14:53:16 +0900

    メール送信検証中_アドレスをto_sで変換してみた

--- app/mailers/posted_mailer.rb
+++ app/mailers/posted_mailer.rb
@@ -3,7 +3,7 @@ class PostedMailer < ApplicationMailer
   def posted_mail_new(picture)
     @picture = picture
 
-    mail to: @picture.user.email , subject:"新規投稿の確認メール"
+    mail to: @picture.user.email.to_s , subject:"新規投稿の確認メール"
   end
 
 
@@ -11,14 +11,14 @@ class PostedMailer < ApplicationMailer
 
     @picture = picture
     @picture_old = picture_old
-
-    mail to: @picture.user.email , subject:"投稿編集の確認メール"
+mail to: "wkym_4a@icloud.com", subject: "お問い合わせの  確認メール"
+    #mail to: @picture.user.email , subject:"投稿編集の確認メール"
   end
 
   def posted_mail_delete(picture)
     @picture = picture
 
-    mail to: @picture.user.email , subject:"投稿削除の確認メール"
+    mail to: @picture.user.email.to_s , subject:"投稿削除の確認メール"
   end
 
 end
